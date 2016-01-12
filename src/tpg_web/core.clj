a337dba25a6cc47fe08c8932ad60c11c81d6f90a
bug2bug <S.Sobko@profitware.ru>
2016-01-13 02:19:19 +0300

    Menus JSONP

--- src/tpg_web/core.clj
+++ src/tpg_web/core.clj
@@ -2,9 +2,11 @@
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
+            [clojure.data.json :as json]
             [clojure.java.io :as io]
             [clojure.string :as s]
             [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
             [environ.core :refer [env]]
             [tpg-web.page :as page]
             [tpg-web.tpl :as tpl])
@@ -28,7 +30,7 @@
       :contentMenu content-menu
       :contentTitle content-title)))
 
-(defroutes app
+(defroutes app-routes
   (GET (str "/:page-id{|" (s/join "|" page/site-menus) "}")
     [page-id]
     (let [
@@ -40,6 +42,17 @@
         :staticRoot static-root}]
       (tpl/render-page
         (render-map-by-current-map current-map))))
+
+  (GET "/menus.js"
+    []
+    (response/content-type
+      (response/response
+        (str
+          "tpgMenuCallback("
+          (json/write-str page/all-menus-map)
+          ");"))
+      "application/javascript"))
+
   (route/not-found
     (tpl/render-page
       (render-map-by-current-map {
@@ -48,6 +61,8 @@
         :isIndex false
         :staticRoot static-root
         }))))
+
+(def app app-routes)
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]

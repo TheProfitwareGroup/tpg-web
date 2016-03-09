2940069f423a4aa494077c9d44691e606b83ef1d
bug2bug <S.Sobko@profitware.ru>
2016-03-09 23:52:16 +0300

    https - templates

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ru.profitware/tpg-web "0.1.0-SNAPSHOT"
   :description "The Profitware Group Website"
-  :url "http://tpg.su/"
+  :url "https://tpg.su/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
